cec48692242ad4292976cb249bc6577557713830
JohannaOCR <johanna.tristan.OCR@gmail.com>
2023-04-24 21:55:57 +0200

    ajout lombok sur la classe Rules

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/RuleName.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/RuleName.java
@@ -1,27 +1,55 @@
 package com.nnk.springboot.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.Hibernate;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.sql.Timestamp;
+import java.util.Objects;
 
 @Entity
+@Getter
+@Setter
+@ToString
 @Table(name = "rulename")
 public class RuleName {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "Id", nullable = false)
-    Integer id;
-    @Column(name = "name")
-    String name;
-    @Column(name = "description")
-    String description;
-    @Column(name = "json")
-    String json;
-    @Column(name = "template")
-    String template;
-    @Column(name = "sqlStr")
-    String sqlStr;
-    @Column(name = "sqlPart")
-    String sqlPart;
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @JoinColumn(name = "id", nullable = false)
+    private Integer id;
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+    @NotBlank(message = "Description is mandatory")
+    private String description;
+    @NotBlank(message = "Json is mandatory")
+    private String json;
+    @NotBlank(message = "Template is mandatory")
+    private String template;
+    @NotBlank(message = "SqlStr is mandatory")
+    private String sqlStr;
+    @NotBlank(message = "SqlPart is mandatory")
+    private String sqlPart;
 
+    public RuleName() {
+    }
+
+    public RuleName(String name, String description, String json, String template, String sqlStr, String sqlPart) {
+        this.name = name;
+        this.description = description;
+        this.json = json;
+        this.template = template;
+        this.sqlStr = sqlStr;
+        this.sqlPart = sqlPart;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        RuleName ruleName = (RuleName) o;
+        return id != null && Objects.equals(id, ruleName.id);
+    }
 }
