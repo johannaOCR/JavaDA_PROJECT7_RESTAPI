b77a959684fd24aab6317da38d87bdacf77ff3e8
JohannaOCR <johanna.tristan.OCR@gmail.com>
2023-05-04 00:00:27 +0200

    Correction controller + test fonctionnel OK

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/CurveController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/CurveController.java
@@ -27,7 +27,7 @@ public class CurveController {
     }
 
     @GetMapping("/curvePoint/add")
-    public String addBidForm(CurvePoint bid) {
+    public String addBidForm(CurvePoint curvePoint) {
         return "curvePoint/add";
     }
 
