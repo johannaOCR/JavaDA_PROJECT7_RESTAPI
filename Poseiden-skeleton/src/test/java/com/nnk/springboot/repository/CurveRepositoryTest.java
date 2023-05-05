00f7194b8730a894b0f3405f83ae4b3459462150
JohannaOCR <johanna.tristan.OCR@gmail.com>
2023-05-05 10:50:38 +0200

    Ajout test sur repository

---
+++ Poseiden-skeleton/src/test/java/com/nnk/springboot/repository/CurveRepositoryTest.java
@@ -0,0 +1,46 @@
+package com.nnk.springboot.repository;
+
+import com.nnk.springboot.domain.CurvePoint;
+import com.nnk.springboot.repositories.CurvePointRepository;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import java.util.Optional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CurveRepositoryTest {
+
+    @Autowired
+    private CurvePointRepository curvePointRepository;
+
+    @Test
+    public void curvePointTest() {
+        CurvePoint curvePoint = new CurvePoint(1, 10d, 10d);
+
+        // Save
+        curvePoint = curvePointRepository.save(curvePoint);
+        Assert.assertNotNull(curvePoint.getId());
+        Assert.assertTrue(curvePoint.getCurveId() == 1);
+
+        // Update
+        curvePoint.setCurveId(2);
+        curvePoint = curvePointRepository.save(curvePoint);
+        Assert.assertTrue(curvePoint.getCurveId() == 2);
+
+        // Find
+        List<CurvePoint> listResult = curvePointRepository.findAll();
+        Assert.assertTrue(listResult.size() > 0);
+
+        // Delete
+        Integer id = curvePoint.getId();
+        curvePointRepository.delete(curvePoint);
+        Optional<CurvePoint> curvePointList = curvePointRepository.findById(id);
+        Assert.assertFalse(curvePointList.isPresent());
+    }
+}
