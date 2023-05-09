ae83795a1481c880702026ce6ccb0bb1a37f48cf
JohannaOCR <johanna.tristan.OCR@gmail.com>
2023-05-09 22:29:19 +0200

    Implémentation de RuleNameService et des tests

---
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/services/RuleNameService.java
@@ -0,0 +1,41 @@
+package com.nnk.springboot.services;
+
+import com.nnk.springboot.domain.RuleName;
+import com.nnk.springboot.repositories.RuleNameRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class RuleNameService {
+    private static final Logger logger = LogManager.getLogger("RuleNameService");
+    @Autowired
+    private RuleNameRepository ruleNameRepository;
+
+    public List<RuleName> getAllRuleName(){
+        return ruleNameRepository.findAll();
+    }
+
+    public void addRuleName(RuleName ruleName){
+        logger.info("Adding ruleName");
+        ruleNameRepository.save(ruleName);
+    }
+
+    public void updateRuleName(RuleName ruleName){
+        logger.info("Updating ruleName");
+        ruleNameRepository.save(ruleName);
+    }
+
+    public void deleteRuleName(Integer id){
+        logger.info("Deleting ruleName");
+        ruleNameRepository.deleteById(id);
+    }
+
+    public Optional<RuleName> getRuleNameById(Integer id) {
+        return ruleNameRepository.findById(id);
+    }
+}
---
+++ Poseiden-skeleton/src/test/java/com/nnk/springboot/service/RuleNameServiceTest.java
@@ -0,0 +1,80 @@
+package com.nnk.springboot.service;
+
+import com.nnk.springboot.domain.RuleName;
+import com.nnk.springboot.repositories.RuleNameRepository;
+import com.nnk.springboot.services.RuleNameService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+@RunWith(SpringRunner.class)
+public class RuleNameServiceTest {
+    @InjectMocks
+    private RuleNameService ruleNameService;
+    @Mock
+    RuleNameRepository ruleNameRepository;
+
+    @Test
+    public void addRuleNameTest() {
+        //GIVEN
+        RuleName ruleName = new RuleName("Rule Name", "Description", "Json",
+                "Template", "SQL", "SQL Part");
+        //WHEN
+        ruleNameService.addRuleName(ruleName);
+        //THEN
+        verify(ruleNameRepository,times(1)).save(ruleName);
+    }
+    @Test
+    public void updateRuleNameTest() {
+        //GIVEN
+        RuleName ruleName = new RuleName("Rule Name", "Description", "Json",
+                "Template", "SQL", "SQL Part");
+        //WHEN
+        ruleNameService.updateRuleName(ruleName);
+        //THEN
+        verify(ruleNameRepository,times(1)).save(ruleName);
+    }
+    @Test
+    public void getAllRuleNameTest() {
+        //WHEN
+        ruleNameService.getAllRuleName();
+        //THEN
+        verify(ruleNameRepository,times(1)).findAll();
+    }
+    @Test
+    public void getRuleNameByIdTest() {
+        //GIVEN
+        RuleName ruleName = new RuleName("Rule Name", "Description", "Json", "Template", "SQL", "SQL Part");
+        ruleName.setId(1);
+        Integer id = ruleName.getId();
+        //WHEN
+        when(ruleNameRepository.findById(id)).thenReturn(Optional.of(ruleName));
+        RuleName result = ruleNameService.getRuleNameById(1).get();
+        //THEN
+        verify(ruleNameRepository,times(1)).findById(1);
+        assertThat(result.getId()).isEqualTo(1);
+        assertThat(result.getName()).isEqualTo("Rule Name");
+        assertThat(result.getDescription()).isEqualTo("Description");
+        assertThat(result.getJson()).isEqualTo("Json");
+        assertThat(result.getTemplate()).isEqualTo("Template");
+        assertThat(result.getSqlStr()).isEqualTo("SQL");
+        assertThat(result.getSqlPart()).isEqualTo("SQL Part");
+    }
+    @Test
+    public void deleteByIdRuleNameTest() {
+        //GIVEN
+        RuleName ruleName = new RuleName("Rule Name", "Description", "Json", "Template", "SQL", "SQL Part");
+        ruleName.setId(1);
+        //WHEN
+        ruleNameService.deleteRuleName(1);
+        //THEN
+        verify(ruleNameRepository,times(1)).deleteById(1);
+    }
+}
